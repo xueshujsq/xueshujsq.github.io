cd559d4029cd7e6e83e55915b8cb1ebea75f2b72
xueshujsq <73819902+xueshujsq@users.noreply.github.com>
2020-11-16 08:42:50 +0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,16 @@
+window.settings = {
+  // 站点标题
+  title: 'Test',
+  // 站点描述
+  description: 'HAHA',
+  // API
+  host: 'https://xueshu.icu',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'default'
+  },
+  // 背景
+  background_url: ''
+}
