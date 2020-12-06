57518ed62e1e4e6586f4c3caf2c22da608012ba5
xueshujsq <73819902+xueshujsq@users.noreply.github.com>
2020-12-06 12:11:57 +0800

    Update env.js

--- env.js
+++ env.js
@@ -1,16 +1,1 @@
-window.settings = {
-  // 站点标题
-  title: '学术加速器',
-  // 站点描述
-  description: '仅用于学习、娱乐、外贸及工作用途，请遵守中华人民共和国法律法规,切勿在任何地方发布分裂国家，激化民族矛盾的言论，极端政治人士请绕道。 若不同意，请马上关闭页面。',
-  // API
-  host: 'https://xueshujsq.top',
-  // 主题
-  theme: {
-    sidebar: 'light',
-    header: 'dark',
-    color: 'darkblue'
-  },
-  // 背景
-  background_url: 'https://cdn.jsdelivr.net/gh/xueshujsq/xueshujsq.github.io@main/static/background.jpg'
-}
+window.settings = {title: '学术加速器',description: '仅用于学习、娱乐、外贸及工作用途，请遵守中华人民共和国法律法规,切勿在任何地方发布分裂国家，激化民族矛盾的言论，极端政治人士请绕道。 若不同意，请马上关闭页面。',host: 'https://xueshujsq.top',theme: {sidebar: 'light',header: 'dark',color: 'darkblue'},background_url: 'https://cdn.jsdelivr.net/gh/xueshujsq/xueshujsq.github.io@main/static/background.jpg'}
